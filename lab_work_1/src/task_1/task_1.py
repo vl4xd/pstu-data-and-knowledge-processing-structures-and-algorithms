0af5a231ad8976ee1ce8a91250fe5f5870d7e405
Platonov Vladislav <vlaxddd@yandex.ru>
2026-02-25 22:15:46 +0500

    init

---
+++ lab_work_1/src/task_1/task_1.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+
+def my_multisets_intersection(a: list[int], b: list[int]) -> list[int]:
+    counter_a, counter_b = Counter(a), Counter(b)
+    c: list[int] = []
+    for k, v in counter_a.items():
+        if k not in counter_b:
+            continue
+        if v < counter_b[k]:
+            c += [k] * v
+        else:
+            c += [k] * counter_b[k]
+    return c
+
+if __name__ == '__main__':
+    res = f'''
+Пример работы функции (my_multisets_intersection):
+a = [1, 2, 3, 4, 4]
+b = [3, 3, 4, 5, 6]
+c = {my_multisets_intersection([1, 2, 3, 4, 4], [3, 3, 4, 5, 6])}
+
+Запустите тесты (test_task_1.py) для рассмотрения множества примеров
+'''
+    print(res)
---
+++ lab_work_1/src/task_1/test_task_1.py
@@ -0,0 +1,27 @@
+import unittest
+from task_1 import my_multisets_intersection
+
+
+class TestMyMultisetsIntersection(unittest.TestCase):
+
+    def test_both_empty(self):
+        self.assertCountEqual(my_multisets_intersection([], []), [])
+
+    def test_one_empty(self):
+        self.assertCountEqual(my_multisets_intersection([1, 2], []), [])
+        self.assertCountEqual(my_multisets_intersection([], [1, 2]), [])
+
+    def test_no_common_elements(self):
+        self.assertCountEqual(my_multisets_intersection([1, 2, 3], [4, 5, 6]), [])
+
+    def test_identical_lists(self):
+        self.assertCountEqual(my_multisets_intersection([1, 2, 2, 3], [1, 2, 2, 3]), [1, 2, 2, 3])
+        self.assertCountEqual(my_multisets_intersection([3, 2, 2, 3], [2, 3, 2, 3]), [2, 2, 3, 3])
+
+    def test_different_counts(self):
+        self.assertCountEqual(my_multisets_intersection([1, 1, 1, 2], [1, 1, 3]), [1, 1])
+        self.assertCountEqual(my_multisets_intersection([1, 1], [1, 1, 1]), [1, 1])
+        self.assertCountEqual(my_multisets_intersection([3, 2, 2, 3], [1, 2, 3, 2, 3]), [2, 2, 3, 3])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
